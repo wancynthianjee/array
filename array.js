6052f1fbecf5dd933c2eff49b8759fb9d3f4aff9
wancynthianjeri <wanjirucynthianjeri@gmail.com>
2023-03-15 17:50:49 +0300

    commit work

---
+++ array.js
@@ -0,0 +1,61 @@
+// Find the last element of the following arrays.
+ let arr1 = [3,7,34,90,12];
+ let arr2 = [true, "green", "where",12,56];
+ let lastElement = arr1.pop();
+ console.log(lastElement);
+ let Elementlast = arr2.pop();
+ console.log(Elementlast);
+
+
+
+// Write a JS program that will join the following array elements into a string
+myPets = ["Cow", "Bird", "Snake", "Dog"];
+console.log(myPets.join());
+// console.log(myPets.join());
+// console.log(myPets.join('+'));
+
+
+// Write a JS script to sort the following array items
+var arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
+arr3.sort(function(a, b){return b-a});
+console.log(arr3)
+
+// Write a program to remove duplicates from the following array.
+// Console the array without duplicates, and 
+// nsole another array that only contains the duplicates
+// var arr = ["apple", "mango", "apple","orange", "mango", "mango"];
+//   const a=["apple", "mango", "apple","orange", "mango", "mango"];
+//   const b=["apple", "mango", "apple","orange", "mango", "mango"];
+//    const c=["apple", "mango", "apple", "mango", "mango"];
+// var unq=aar
+//   .map((name) => {
+//     return {count: 1, name: name}
+//   })
+//   .reduce((a, b) => {
+//     a[b.name] = (a[b.name] || 0) + b.count
+//     return a
+//   }, {})
+
+// var sorted = Object.keys(uniq).sort((a, b) => uniq[a] < uniq[b])
+
+// console.log(sorted)
+
+//     Write a JS script to search for the following word in the array.
+// "way"
+// If the word is present, console it else console "the search word was not found"
+let arr5 = ["the", "way", "x", 4];
+if(arr5==["way"]){
+    console.log("way")
+}
+else{
+    (arr5=["way"])
+    console.log("way")
+}
+
+// Write a JS script to sort the following string
+let word = "sevink"
+console.log(word.split('').sort().join(''));
+
+
+
+
